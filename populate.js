0c8e204622c87bd8444fe17fac4d8f6fef9b238d
Ayomide <ilesanmibry@gmail.com>
2023-06-08 10:02:16 +0100

    Mock Database

---
+++ populate.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+import Product from './models/products.js';
+import { connectDB } from './db/connect.js';
+import jsonProducts from './data/products.json' assert { 'type': 'json' };
+
+
+const app = express()
+dotenv.config()
+
+const start = async () => {
+  try {
+    connectDB()
+
+    // await Product.deleteMany()
+    await Product.create(jsonProducts)
+    console.log("Populate connected successfully");
+    process.exit(0)
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start()
