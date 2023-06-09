51d8a1f450da97bd7b20da9a2ede63b0a0118edf
Ayomide <ilesanmibry@gmail.com>
2023-06-09 18:55:12 +0100

    feat: Add route handler for '/' path to serve index.html

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express  from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import 'express-async-errors'
+import cors from 'cors'
+import path from 'path'
 
 
 import productRoute from './routes/productRoutes.js'
@@ -19,8 +21,25 @@ mongoOff
 
 // Middlewares
 app.use(express.json())
+app.use(cors())
+app.use(express.static('public'));
 
 // Routes
+//Root route
+// app.get('/', (req, res) => {
+//     res.status(300).redirect('/index.html');
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+//   //__dirname : It will resolve to your project folder.
+// });
+
+app.get('/',  (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
 app.use('/api/v1/products', productRoute)
 
 // Middlewares
